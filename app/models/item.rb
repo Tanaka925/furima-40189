9821c4652dcf6aae01ba527ac5b5aa5056744453
Tanaka925 <tawashi259@gmail.com>
2024-02-18 08:50:15 +0900

    アソシエーションとバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  validates :product_name,       presence: true
+  validates :description,        presence: true
+  validates :category_id,        presence: true
+  validates :condition_id,       presence: true
+  validates :delivery_charge_id, presence: true
+  validates :area_id,            presence: true
+  validates :shipping_day_id,    presence: true
+  validates :price,              presence: true
+
+  belongs_to :user
 end
