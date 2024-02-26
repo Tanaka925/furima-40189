d84f82ae7d82f7a2189da0541003f74efd01b700
Tanaka925 <tawashi259@gmail.com>
2024-02-26 11:28:01 +0900

    ActiveHashを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @item = Item.order("created_at DESC")
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render_to :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product_name, :description, :category_id, :condition_id, :delivery_charge_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,21 @@
 class Item < ApplicationRecord
   validates :product_name,       presence: true
   validates :description,        presence: true
-  validates :category_id,        presence: true
-  validates :condition_id,       presence: true
-  validates :delivery_charge_id, presence: true
+  validates :category_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :area_id,            presence: true
   validates :shipping_day_id,    presence: true
   validates :price,              presence: true
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :area
+  belongs_to :shipping_day
+
 end
