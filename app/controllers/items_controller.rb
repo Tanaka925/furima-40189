badfc71167b61d8d0a8c126ff1584b9ffd308586
Tanaka925 <tawashi259@gmail.com>
2024-03-09 13:41:48 +0900

    編集機能　updateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
