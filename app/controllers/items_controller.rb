ab65f70f35cde7260e1288c64bb71248e6201f16
Tanaka925 <tawashi259@gmail.com>
2024-03-12 14:31:50 +0900

    編集機能　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.order(created_at: :desc)
