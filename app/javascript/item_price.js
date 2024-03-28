2390b366859ef59425db10e0c0ce035ba31a836b
Tanaka925 <tawashi259@gmail.com>
2024-03-28 11:28:41 +0900

    JavaScript 読み込みの安定化

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,17 @@
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
 
-  const inputValue = parseFloat(priceInput.value);
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const inputValue = parseFloat(priceInput.value);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
 
-  const addTaxValue = parseFloat(addTaxDom.innerHTML);
-  const profitValue = document.getElementById("profit");
-  profitValue.innerHTML = Math.floor(inputValue - addTaxValue)
+    const addTaxValue = parseFloat(addTaxDom.innerHTML);
+    const profitValue = document.getElementById("profit");
+    profitValue.innerHTML = Math.floor(inputValue - addTaxValue)
 
-});
+  });
+};
 
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
